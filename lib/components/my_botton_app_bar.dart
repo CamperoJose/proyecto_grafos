159829f2bb0738f5770eb4bb3217f936866a212d
Moras123 <diego.moron@ucb.edu.bo>
2023-10-09 09:22:08 -0400

    espacios

--- lib/components/my_botton_app_bar.dart
+++ lib/components/my_botton_app_bar.dart
@@ -52,7 +52,7 @@ class MyBottomAppBar extends StatelessWidget {
             ),
           ),
         ],
-      ),
-    );
-  }
+     ),
+    );
+  }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -259,7 +259,7 @@ class HomePage extends StatelessWidget {
             ],
           ),
         ),
-      ),
-    );
-  }
+     ),
+    );
+  }
 }
