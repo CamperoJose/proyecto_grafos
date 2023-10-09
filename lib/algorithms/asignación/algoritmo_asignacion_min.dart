4c54294a3db42f098c40271be7cce8992bf7e38f
CamperoJose <jose.campero@ucb.edu.bo>
2023-10-09 08:52:02 -0400

    agregando logica de asignacion (max y min)

---
+++ lib/algorithms/asignación/algoritmo_asignacion_min.dart
@@ -0,0 +1,31 @@
+List<int> hungarianAlgorithm(List<List<int>> matrix) {
+  List<int> result = [];
+  for (int i = 0; i < matrix.length; i++) {
+    if (todoCero(matrix[i])) {
+      result.add(-1);
+    } else {
+      result.add(posMenor(matrix[i]));
+    }
+  }
+
+  return result;
+}
+
+bool todoCero(List<int> fila) {
+  for (int i = 0; i < fila.length; i++) {
+    if (fila[i] != 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+int posMenor(List<int> fila) {
+  int m = 99999999;
+  for (int i = 0; i < fila.length; i++) {
+    if (fila[i] < m && fila[i] != 0) {
+      m = fila[i];
+    }
+  }
+  return fila.indexOf(m);
+}
